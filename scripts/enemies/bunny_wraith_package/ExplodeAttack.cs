d813821779dcba6475c2ce515d38ef1f5d67baab
AdamFrenzen <adam.frenzen@gmail.com>
2025-11-15 13:17:07 -0700

    feat: improve progress bars

--- scripts/enemies/bunny_wraith_package/ExplodeAttack.cs
+++ scripts/enemies/bunny_wraith_package/ExplodeAttack.cs
@@ -3,30 +3,30 @@ using System;
 
 public partial class ExplodeAttack : Node2D
 {
-    [Signal]
-    public delegate void AttackFinishedEventHandler();
+	[Signal]
+	public delegate void AttackFinishedEventHandler();
 
-    [Export] private GpuParticles2D _explodeEffect;
-    [Export] private PointLight2D _explodeLight;
-    [Export] private float _lightIntensity;
+	[Export] private GpuParticles2D _explodeEffect;
+	[Export] private PointLight2D _explodeLight;
+	[Export] private float _lightIntensity;
 
-    private double _currTime;
+	private double _currTime;
 
-    public override void _Ready()
-    {
-        _explodeLight.Energy = 0;
-        _explodeEffect.Emitting = true;
-        _explodeEffect.Finished += OnFinished;
-    }
+	public override void _Ready()
+	{
+		_explodeLight.Energy = 0;
+		_explodeEffect.Emitting = true;
+		_explodeEffect.Finished += OnFinished;
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        _currTime = Math.Clamp(_currTime + delta, 0, .2);
-        _explodeLight.Energy = (float)Math.Sin(_currTime/.2 * Mathf.Pi) * _lightIntensity;
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		_currTime = Math.Clamp(_currTime + delta, 0, .2);
+		_explodeLight.Energy = (float)Math.Sin(_currTime/.2 * Mathf.Pi) * _lightIntensity;
+	}
 
-    private void OnFinished()
-    {
-        QueueFree();
-    }
+	private void OnFinished()
+	{
+		QueueFree();
+	}
 }
