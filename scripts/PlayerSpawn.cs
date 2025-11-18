aed3457d75c50ffa535cc89ff53ee842a920f5cd
AdamFrenzen <adam.frenzen@gmail.com>
2025-11-17 23:28:16 -0700

    feat: portal sound

--- scripts/PlayerSpawn.cs
+++ scripts/PlayerSpawn.cs
@@ -10,13 +10,18 @@ public partial class PlayerSpawn : Node2D
 	[Export] private PointLight2D light;
 	[Export] private float maxLightEnergy;
 	private double _currTime;
+	
+	private AudioStreamPlayer2D _audioStream;
 
 	public override void _Ready()
-	{
+	{	
+		_audioStream = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+		FadeInSound();
 		light.Energy = 0;
 		particles.Emitting = true;
-		particles.Finished += QueueFree;
+		particles.Finished += Finished;
 	}
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		_currTime += delta;
@@ -26,5 +31,32 @@ public partial class PlayerSpawn : Node2D
 		{
 			EmitSignalPortalOpen();
 		}
+	}
+	
+	private void Finished()
+	{
+		FadeOutSoundAndFree();
+	}
+	
+	public async void FadeInSound(float duration = 1.5f, float targetVolume = -10f)
+	{
+		_audioStream.VolumeDb = -40f;       // start very quiet
+		_audioStream.Play();
+		
+		var tween = CreateTween();
+		tween.TweenProperty(_audioStream, "volume_db", targetVolume, duration);
+		
+		await ToSignal(tween, "finished");
+	}
+	
+	public async void FadeOutSoundAndFree(float duration = 1.5f)
+	{
+		var tween = CreateTween();
+		tween.TweenProperty(_audioStream, "volume_db", -40f, duration);
+
+		await ToSignal(tween, "finished");
+		_audioStream.Stop();
+		
+		QueueFree();
 	}
 }
